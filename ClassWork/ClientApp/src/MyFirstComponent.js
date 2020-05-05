49f380eb09ea741006f617c494135c22e7716652
necha <nechamagross17@gmail.com>
2020-05-05 15:18:06 -0700

    Add project files.

---
+++ ClassWork/ClientApp/src/MyFirstComponent.js
@@ -0,0 +1,14 @@
+﻿import React from 'react';
+import MySecondComponent from './MySecondComponent';
+
+class MyFirstComponent extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello from My First component</h1>
+                <MySecondComponent />
+            </div>
+        );
+    }
+}
+export default MyFirstComponent; 
---
+++ ClassWork/ClientApp/src/MySecondComponent.js
@@ -0,0 +1,7 @@
+﻿import React from 'react';
+class MySecondComponent extends React.Component {
+    render() {
+        return <p>Hello From my Second component</p>
+    }
+}
+export default MySecondComponent;
